fd7c049fe23293e98536e7b92c1e1cf0fa1a6bed
sudeep-o2 <sudeepsavai16@gmail.com>
2024-06-13 23:03:45 +0530

    params

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const app = express();
 
 app.use(express.json());
 
-
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
-
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
 // app.get('/',(req,res) => {
 //     res.status(200).json({message:'hello from server',name:'lord'});
@@ -16,26 +16,59 @@ const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simpl
 //     res.send('u can post data here');
 // });
 
-app.get('/api/v1/tours',(req,res) => {
-    res.status(200).json({
-        status:'success',
-        results:tours.length,
-        data:{
-            tours:tours
-        }
-    });
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
 });
 
-app.post('/api/v1/tours',(req,res) => {
-    console.log(req.body());
-    res.send('done');
-})
+app.get('/api/v1/tours/:id', (req, res) => {
+  //   console.log(req.params);
+  //   console.log(req.params.id);
 
+  const tour = tours.find((el) => el.id === parseInt(req.params.id));
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'failed',
+      message: 'InvalidId',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tours: newTour,
+        },
+      });
+    }
+  );
+});
 
 const port = 3000;
 
-app.listen(port,() => {
-    console.log(`app running on port ${port}`);
-});     
-
-
+app.listen(port, () => {
+  console.log(`app running on port ${port}`);
+});
