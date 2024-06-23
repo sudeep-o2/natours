725664d05e3aa58b3b2b3e60e5b2bd2f8f1d8170
sudeep-o2 <sudeepsavai16@gmail.com>
2024-06-23 21:05:04 +0530

    import data

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,15 +5,52 @@ const tourSchema = new Mongoose.Schema({
     type: String,
     required: [true, 'A tour should be defined'],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have durations'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: ['A tour must have a difficulty'],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a summary'],
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a coverimage'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = Mongoose.model('Tour', tourSchema);
