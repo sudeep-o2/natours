b532dc8eca99c7f2c933abac32993f9277296006
sudeep-o2 <sudeepsavai16@gmail.com>
2024-07-02 10:18:29 +0530

    document,query,aggregation middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const Mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new Mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const tourSchema = new Mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have durations'],
@@ -53,6 +55,10 @@ const tourSchema = new Mongoose.Schema(
       select: false,
     },
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     toJSON: { virtuals: true },
@@ -62,6 +68,44 @@ const tourSchema = new Mongoose.Schema(
 
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
+});
+
+// Mongoose
+// Document middleware - runs before .save() and .create()  also called as (pre hook)
+tourSchema.pre('save', function (next) {
+  //console.log(this);
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// post middleware
+
+// tourSchema.post('save', function (doc, next) {
+//   console.log(this);
+//   next();
+// });
+
+// Query Middleware
+
+//tourSchema.pre('find', function (next) {
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function (doc, next) {
+  console.log(`time taken for Query to execute is ${Date.now() - this.start} `);
+  //console.log(doc);
+  next();
+});
+
+// Aggregation pipeline
+
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  console.log(this.pipeline());
+  next();
 });
 
 const Tour = Mongoose.model('Tour', tourSchema);
