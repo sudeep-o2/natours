c37f8bfae63bebe8d3dcea45be38ad3fbef69fb7
sudeep-o2 <sudeepsavai16@gmail.com>
2024-07-20 12:03:48 +0530

    signup and login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,19 @@
 const fs = require('fs');
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
 
 // const tours = JSON.parse(
 //   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`),
 // );
 
-exports.getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'this route is not yet defined',
+exports.getAllUsers = catchAsync(async (req, res) => {
+  const users = await User.find();
+
+  res.status(200).json({
+    status: 'success',
+    users,
   });
-};
+});
 
 exports.getUser = (req, res) => {
   res.status(500).json({
