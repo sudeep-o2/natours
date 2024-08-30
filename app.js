f0dc05ba59476b3d6c72616d9b1e3dc095f8c1ef
sudeep-o2 <sudeepsavai16@gmail.com>
2024-08-30 10:37:40 +0530

    building map according to locations using mapbox

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const helmet = require('helmet');
 const mongosanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -33,6 +34,8 @@ app.use(helmet());
 
 // body parser , reading data from body into req.body
 app.use(express.json({ limit: '10kb' })); // inside bracket is not mandatory
+
+app.use(cookieParser());
 
 // Data sanitization against NoSQL querry injection
 app.use(mongosanitize());
@@ -72,7 +75,7 @@ app.use('/api', limiter);
 // test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  // console.log(req.headers);
+  console.log(req.cookies);
   next();
 });
 
