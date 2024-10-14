3e974412e84ed27aa08ba1e0ecbd0fbcb5efcc94
sudeep-o2 <sudeepsavai16@gmail.com>
2024-10-14 16:01:30 +0530

    creating new bookings on checkout success

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const viewsController = require('../controllers/viewsController');
 const authController = require('../controllers/authController');
+const bookingController = require('../controllers/bookingController');
 
 const router = express.Router();
 
@@ -10,7 +11,12 @@ const router = express.Router();
 
 // routes
 
-router.get('/', authController.isLoggedIn, viewsController.getOverview);
+router.get(
+  '/',
+  bookingController.createBookingCheckout,
+  authController.isLoggedIn,
+  viewsController.getOverview,
+);
 
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 
